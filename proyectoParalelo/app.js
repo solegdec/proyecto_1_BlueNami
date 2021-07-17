5c4d26d36df4097771c20349e1b09c7cc018f9e6
solegdec <86257609+solegdec@users.noreply.github.com>
2021-07-17 16:00:48 -0300

    Add files via upload

---
+++ proyectoParalelo/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3030
+const path = require('path')
+
+app.get('/', function(req, res) {
+    res.send('Hola mundo!')
+})
+
+app.get('/home', function(request, response) {
+    response.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+app.use(express.static('public'))
+
+app.get('*', function(request, response) {
+    response.send('NOT FOUND', 404)
+})
+
+app.listen(port, () => {
+    console.log('La app esta funcionado en http://localhost:' + port)
+})
