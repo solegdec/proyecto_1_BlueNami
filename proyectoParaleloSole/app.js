8b60f55f74b6057dc388f8636d6a4b953e16b6a2
Sole <soledecono@hotmail.com>
2021-07-27 17:32:58 -0300

    renamed:    ../proyectoParalelo/app.js -> app.js 	renamed:    ../proyectoParalelo/package-lock.json -> package-lock.json 	renamed:    ../proyectoParalelo/package.json -> package.json 	new file:   public/css/style.css 	renamed:    ../proyectoParalelo/public/img/.DS_Store -> public/img/.DS_Store#	renamed:    ../proyectoParalelo/public/img/carrobm.png -> public/img/carrobm.png 	renamed:    ../proyectoParalelo/public/img/carronm.png -> public/img/carron.png 	renamed:    ../proyectoParalelo/public/img/logoBN.png -> public/img/logoBNpng 	new file:   public/img/olasSole.png 	new file:   public/img/olasSole1000x50.png 	new file:   public/img/olasSole576x70.png 	new file:   public/img/tabla1chica.png 	new file:   public/img/tabla1grandeSG.png 	new file:   public/img/tabla2.png new file:   public/img/tabla3.png 	new file:   public/img/tabla4.png 	new file:   public/img/tablaVector.png 	new file:   public/img/tablaVector50x62 (1) (1).png 	new file:   public/img/tablaVector50x62 (1).png 	new file:   public/img/tablaVector50x62.png 	new file:   views/index.html 	new file:   views/productCart3.html

---
+++ proyectoParaleloSole/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3030
+const path = require('path')
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'views/productCart3.html'))
+})
+
+app.use(express.static('public'))
+
+app.get('*', function(request, response) {
+    response.send('NOT FOUND', 404)
+})
+
+app.listen(port, () => {
+    console.log('La app esta funcionado en http://localhost:' + port)
+})
