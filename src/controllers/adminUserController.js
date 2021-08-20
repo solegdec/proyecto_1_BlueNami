b49879451adc1a5113907d4a57a5b88685b5fe81
gzanol <ginazanol@gmail.com>
2021-08-19 21:04:29 -0300

    saque las rutas del controlador, los habia puesto para comparar

--- src/controllers/adminUserController.js
+++ src/controllers/adminUserController.js
@@ -50,9 +50,3 @@ const adminUserController={
 }
 module.exports= adminUserController;
 
-router.get ("/adminUser", adminUserController.entrarAdminUser);
-router.get ("/adminAddUser", adminUserController.entrarAdminAddUser);
-router.post ("/adminAddUser", adminUserController.crearAdminAddUser);
-router.get ("/adminModUser", adminUserController.entrarAdminModUser);
-router.put ("/adminModUser", adminUserController.editarAdminAddUser);
-router.delete ("/adminUser", adminUserController.borrarAdminAddUser);
