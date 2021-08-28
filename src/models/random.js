27beb73b401c53c0bb077d4b31df29c317f12ec1
Sebastian Uson <sebastianuson@googlemail.com>
2021-08-28 11:06:10 -0300

    agregue una funcion random para que nos traiga tablas random para mostar tanto en el carrusel como en los destacados del productdetail

---
+++ src/models/random.js
@@ -0,0 +1,17 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+let ran;
+ran = getRandomInt(0, 10);
+let array = [ran];
+for (let i = 0; i < 2; i++) {
+    ran = getRandomInt(0, 10);
+    if (array.length < 2 && array[i] != ran) {
+        array.push(ran);
+    } else if (array.length < 3 && array[i] != ran) {
+        array.push(ran);
+    }
+
+
+}
+module.exports = array;
