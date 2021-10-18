ae2351343405b3a397a0a430a231ec0018b3fcb0
mariabevegni <mariabevegni@gmail.com>
2021-10-18 15:25:14 -0300

    middlewares del User Router hechos. solo falta el authMiddleware de la ruta profile

--- src/middlewares/regFormMidd.js
+++ src/middlewares/regFormMidd.js
@@ -2,15 +2,15 @@ const path = require('path');
 const {check}=require("express-validator")
 
 const validations=[
-    check("nombre").notEmpty().withMessage("Tienes que completar este campo"),
-    check("apellido").notEmpty().withMessage("Tienes que completar este campo"),
-    check("fechaNac").notEmpty().withMessage("Tienes que completar este campo"),
+    check("nombre").notEmpty().withMessage("Tienes que completar el campo Nombre"),
+    check("apellido").notEmpty().withMessage("Tienes que completar el campo Apellido"),
+    check("fechaNac").notEmpty().withMessage("Tienes que completar tu Fecha de Nacimient"),
     check("email")
-    .notEmpty().withMessage("Tienes que completar este campo").bail()
-    .isEmail().withMessage("Tienes que completar con un email"),
-    check("pais").notEmpty().withMessage("Tienes que completar este campo"),
-    check("genero").notEmpty().withMessage("Tienes que completar este campo"),
-    check("password").notEmpty().withMessage("Tienes que completar este campo"),
+    .notEmpty().withMessage("Tienes que completar tu email").bail()
+    .isEmail().withMessage("Tienes que completar con un email valido"),
+    check("pais").notEmpty().withMessage("Tienes que completar tu pais"),
+    check("genero").notEmpty().withMessage("Tienes que completar tu genero"),
+    check("password").notEmpty().withMessage("Tienes que completar la password"),
     check("avatar").custom((value,{req})=>{
         let file=req.file;
         if (!file){
