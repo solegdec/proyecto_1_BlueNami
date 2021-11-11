6d65ded20d342e071104469dff6c6f6d2814a1c5
solegdec <soledecono@hotmail.com>
2021-11-11 18:22:49 -0300

    .

---
+++ public/js/cart.js
@@ -0,0 +1,12 @@
+
+
+window.addEventListener("load",function(){
+      
+    let form = document.querySelector(".productCart") 
+    
+    
+        form.addEventListener("submit",function(){
+alert("GRACIAS POR TU COMPRA!!,Te enviaremos los detalles por mail")
+
+        });
+    })
