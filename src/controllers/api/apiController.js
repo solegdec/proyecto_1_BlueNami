eb4693cb3602ee910ea7945e8303397aa87ad08f
mariabevegni <mariabevegni@gmail.com>
2021-11-27 18:17:00 -0300

    commit

--- src/controllers/api/apiController.js
+++ src/controllers/api/apiController.js
@@ -6,8 +6,8 @@ let apiController={
 listProducts: async (req,res)=>{
     let products=await db.Products.findAll({
         include:["marca","colours"],
-        
-})
+    })
+
     let coloursList= await db.Colours.findAll({
         include:["productos"]
     })
@@ -19,6 +19,7 @@ listProducts: async (req,res)=>{
             count: colour.productos.length
         }
     })
+
     let lastProducts = await db.Products.findAll({
         include:["marca"],
         order: [
@@ -31,19 +32,14 @@ listProducts: async (req,res)=>{
         meta:{
             status: 200,
             count: products.length,
-            
             countByColours: qtyProducts,
             url: "/api/products",
             lastProducts: lastProducts,
-            
-           
         },
         data:products
     }
     res.json(productsJson)
 
-        
-        
     },
     
 
@@ -51,6 +47,7 @@ listColours: async function(req, res){
         let coloursList = await db.Colours.findAll({
             include: ["productos"]
         })
+
         let qtyProducts = coloursList.map(colour =>{
             return {
                 id: colour.id,
@@ -169,7 +166,9 @@ listUsers:(req,res)=>{
 },
 detailUser:(req,res)=>{
 db.Users
-    .findByPk(req.params.id)
+    .findByPk(req.params.id, {
+        include:["categoria"]
+})
     .then(user=>{
         return res.status(200).json({
             data: user,
