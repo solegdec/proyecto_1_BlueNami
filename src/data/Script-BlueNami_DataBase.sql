2d8e68ec25d6afb56827b42b7f348c2e9fdb80a2
gzanol <ginazanol@gmail.com>
2022-02-11 09:51:52 -0300

    .

--- src/data/Script-BlueNami_DataBase.sql
+++ src/data/Script-BlueNami_DataBase.sql
@@ -61,6 +61,7 @@ foto VARCHAR(255) NOT NULL,
 unidades INT UNSIGNED, 
 precio DECIMAL NOT NULL,
 marca_id INT UNSIGNED,
+borrado INT UNSIGNED,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
@@ -159,18 +160,18 @@ insert into `brands`  VALUES
 -- Carga tabla products
 
 insert into `products`  VALUES
- (1, 'Gun', 'nunc donec quis orci eget orci vehicula condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu', 'tabla1chica.png', 73, '12086.93', 10, '2021-05-17 18:52:53', '2021-08-24 13:19:43'),
- (2, 'SuperFish', 'erat fermentum justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis sed ante vivamus', 'tabla2.jpg', 39, '11472.70', 3, '2021-03-15 23:20:10', '2021-05-25 13:09:01'),
- (3, 'Evolutiva', 'ut erat curabitur gravida nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent', 'tabla3.png', 76, '18811.04', 8, '2021-05-15 13:00:17', '2021-06-05 16:06:20'),
- (4, 'SoftBoard', 'in eleifend quam a odio in hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida nisi at', 'tabla3Sole.png', 12, '13144.68', 7, '2021-03-08 08:44:58', '2021-01-12 18:46:21'),
- (5, 'LongBoard', 'nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante', 'tabla4.png', 93, '7672.71', 12, '2021-01-14 14:54:20', '2021-06-25 21:03:05'),
- (6, 'Retro', 'pede venenatis non sodales sed tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel', 'tabla4Sole.png', 14, '17211.16', 6, '2021-07-24 01:47:06', '2021-05-15 11:27:40'),
- (7, 'TwinFin', 'hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula condimentum curabitur in libero ut massa', 'tabla5.png', 95, '19267.68', 3, '2020-09-02 18:29:28', '2021-01-29 14:39:31'),
- (8, 'Gold', 'eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien', 'tabla6.png', 60, '6012.38', 12, '2021-08-16 11:54:17', '2020-12-05 14:28:36'),
- (9, 'Princess', 'ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel', 'tabla7.png', 32, '6630.48', 4, '2021-07-21 20:58:22', '2021-05-03 07:36:03'),
- (10, 'ShortBoard', 'metus aenean fermentum donec ut mauris eget massa tempor convallis nulla neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit', 'tabla2.png', 10, '8888.82', 7, '2020-11-29 16:28:01', '2021-06-18 12:15:14'),
- (11, 'Fish', 'vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec sem duis', 'tabla4.png', 68, '12766.93', 3, '2020-09-22 22:25:35', '2020-09-02 00:17:46'),
- (12, 'Shark', 'sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel nisl', 'tabla2.png', 35, '19280.78', 10, '2021-05-10 15:42:03', '2021-07-11 20:36:04');
+ (1, 'Gun', 'nunc donec quis orci eget orci vehicula condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu', 'tabla1chica.png', 73, '12086.93', 10, 0, '2021-05-17 18:52:53', '2021-08-24 13:19:43'),
+ (2, 'SuperFish', 'erat fermentum justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis sed ante vivamus', 'tabla2.jpg', 39, '11472.70', 3, 0, '2021-03-15 23:20:10', '2021-05-25 13:09:01'),
+ (3, 'Evolutiva', 'ut erat curabitur gravida nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent', 'tabla3.png', 76, '18811.04', 8, 0, '2021-05-15 13:00:17', '2021-06-05 16:06:20'),
+ (4, 'SoftBoard', 'in eleifend quam a odio in hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida nisi at', 'tabla3Sole.png', 12, '13144.68', 7, 0, '2021-03-08 08:44:58', '2021-01-12 18:46:21'),
+ (5, 'LongBoard', 'nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante', 'tabla4.png', 93, '7672.71', 12, 0, '2021-01-14 14:54:20', '2021-06-25 21:03:05'),
+ (6, 'Retro', 'pede venenatis non sodales sed tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel', 'tabla4Sole.png', 14, '17211.16', 6, 0, '2021-07-24 01:47:06', '2021-05-15 11:27:40'),
+ (7, 'TwinFin', 'hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula condimentum curabitur in libero ut massa', 'tabla5.png', 95, '19267.68', 3, 0, '2020-09-02 18:29:28', '2021-01-29 14:39:31'),
+ (8, 'Gold', 'eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien', 'tabla6.png', 60, '6012.38', 12, 0, '2021-08-16 11:54:17', '2020-12-05 14:28:36'),
+ (9, 'Princess', 'ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel', 'tabla7.png', 32, '6630.48', 4, 0, '2021-07-21 20:58:22', '2021-05-03 07:36:03'),
+ (10, 'ShortBoard', 'metus aenean fermentum donec ut mauris eget massa tempor convallis nulla neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit', 'tabla2.png', 10, '8888.82', 7, 0, '2020-11-29 16:28:01', '2021-06-18 12:15:14'),
+ (11, 'Fish', 'vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec sem duis', 'tabla4.png', 68, '12766.93', 3, 0, '2020-09-22 22:25:35', '2020-09-02 00:17:46'),
+ (12, 'Shark', 'sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel nisl', 'tabla2.png', 35, '19280.78', 10, 0, '2021-05-10 15:42:03', '2021-07-11 20:36:04');
 
 
 
@@ -204,10 +205,3 @@ insert into `products_colours`  VALUES
  (15, 9, 1, '2021-08-09 12:03:29', '2021-01-04 03:34:00'),
  (16, 10, 1, '2021-08-09 12:03:29', '2021-01-04 03:34:00'),
  (17, 11, 1, '2021-08-09 12:03:29', '2021-01-04 03:34:00');
-
-
-
-
-
-
--- Borramos el contenido de esta tabla --
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
   "development": {
     "username": "root",
-    "password": "@Mysql07",
+    "password": "gianzari84",
     "database": "blueNami_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
